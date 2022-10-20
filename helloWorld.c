b7ffc08c5321183903f01b7950c63b19006aef25
thitiphongD <thitipsr1504@gmail.com>
2022-10-20 12:52:09 +0700

    lession 1

---
+++ helloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello world! \n");
+
+    printf("Hello!\nMy name is \"Daew\"\n");
+
+    printf("Here is a value: %d\n", 32);
+
+    printf("Everyone knows that %d+%d=%d\n",2,2,4);
+
+    printf("You are a %c %s\n", 'C', "programmer");
+
+    return(0);
+}
---
+++ inputOuput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is printf \n");
+    printf("This is printf 2 \n");
+
+    printf("This is line 1 \nThis is line 2\n");
+
+    return(0);
+}
